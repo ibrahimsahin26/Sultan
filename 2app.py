535fdd1905e02bcebdd237229f3f0cc6435f0a2c
ibrahimsahin26 <ibrahim@hepcazip.com>
2025-06-01 10:03:49 +0300

    Add files via upload

---
+++ 2app.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="Sultan Dağıtım Paneli", layout="centered")
+
+st.markdown("# 🚚 Sultan Dağıtım Paneli")
+st.markdown("Soldaki menüden bir modül seçiniz.")
